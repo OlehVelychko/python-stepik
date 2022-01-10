f962d3b675359dcfdabf9559336ad63b325d0acd
alligator3105 <alligator3105@gmail.com>
2022-01-10 16:50:28 +0200

    khvkhvhvl

--- 5 chapter/ex_5_4_2.py
+++ 5 chapter/ex_5_4_2.py
@@ -1,9 +1,53 @@
+# lst1 = list(map(int, input().split()))
+# lst2 = list()
+# for i, item in enumerate(lst1):
+#     item = item ** 2
+#     lst2.append(item)
+# print(*lst2)
+
+# lst = list(map(int, input().split()))
+# for i, value in enumerate(lst):
+#     lst[i] = value ** 2
+# print(*lst)
+
+# lst = list(map(int, input().split()))
+# lst2 = []
+# for i, value in enumerate(lst):
+#         lst2.append(value)
+#         lst2.append(value)
+# print(*lst2)
+
+#    8 5 32 4
+
+# lst = list(map(float, input().split()))
+# m = lst[1]
+# for i, item in enumerate(lst):
+#     if item < m:
+#         m = item
+# print(m)
+
+# lst = list(map(float, input().split()))
+# for i, item in enumerate(lst):
+#     if item < 0:
+#         lst[i] = -1.0
+# print(*lst)
+
 # 10+25-12
 n = input().replace(' ', '')
-s = n
-res = 0
-for i, item in enumerate(n):
-    if s[i].isdigit():
-        # s1 = n[:i]
-        # n = n[i+1:]
-    print(i, s, n)
+a = n
+n = n.replace('+', '*').replace('-', '*')
+s = n.split('*')
+res = int(s[0])
+b = 1
+for i, item in enumerate(a):
+    if item == '+':
+        res = res + int(s[b])
+        # print(res, b)
+        b = b + 1
+
+    elif item == '-':
+        res = res - int(s[b])
+        # print(res, b)
+        b = b + 1
+
+print(res)
---
+++ 5 chapter/ex_5_5_1.py
@@ -0,0 +1,29 @@
+# cities = input().split()
+# it = iter(cities)
+# print(next(it))
+# print(next(it))
+
+# s = input()
+# it = iter(s)
+# for i in it:
+#     if i != ' ':
+#         print(i, end='')
+#     else:
+#         break
+
+# st_iter = iter(input())
+#
+# n = next(st_iter)
+#
+# while n != ' ':
+#     print(n, end='')
+#     n = next(st_iter)
+
+# a = int(input())
+# it = iter(str(a))
+# for i in it:
+#     print(i, end=' ')
+
+# print(*iter(input()))
+
+print(*input())
